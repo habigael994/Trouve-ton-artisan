d4c1c038fda285c94e08e1fb06a32ea11dc9c8d7
HAB <quentinboyer85@gmail.com>
2024-10-19 01:53:39 +0400

    page artisans

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component' ;
 import { FooterComponent } from './footer/footer.component';
+import { ArtisansComponent } from './artisans/artisans.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,FooterComponent],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent,ArtisansComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/artisans/artisans.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artisans',
+  standalone: true,
+  imports: [],
+  templateUrl: './artisans.component.html',
+  styleUrl: './artisans.component.scss'
+})
+export class ArtisansComponent {
+
+}
