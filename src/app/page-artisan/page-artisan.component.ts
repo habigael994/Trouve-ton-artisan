c2fadcb687bc5502f4e0d9621a94ff435d9c686f
HAB <quentinboyer85@gmail.com>
2024-11-02 23:10:46 +0400

    Ajout du squelette de toutes les pages.

---
+++ src/app/page-artisan/page-artisan.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-artisan',
+  standalone: true,
+  imports: [],
+  templateUrl: './page-artisan.component.html',
+  styleUrl: './page-artisan.component.scss'
+})
+export class PageArtisanComponent {
+
+}
